3f45d33be9f42bd2470da5cc8e94887a57e2d707
cd1989929 <249016429@qq.com>
2014-05-17 11:25:10 +0800

    add access

---
+++ routes/access_controller.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+	var signature = req.param('signature');
+
+	var nonce = req.param('nonce');
+	var timestamp = req.param('timestamp');
+	var token = '';
+	var tempstring = nonce+timestamp+token;
+	var echostr;
+	if(sha1(tempstring)==signature){
+		echostr = req.param('echostr');
+	}else{
+		echostr = req.param('echostr');
+	}
+  	res.send(echostr);
+});
+
+function sha1(content){
+	var crypto = require('crypto');
+	var shasum = crypto.createHash('sha1');
+	shasum.update(content);
+	var d = shasum.digest('hex');
+	return d;
+}
+
+module.exports = router;
